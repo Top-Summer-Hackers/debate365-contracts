48c9e4c4944cdd645addaa332792b942f9a4e5cc
Mehdi Rhouzlane <rhouzm@gmail.com>
2023-05-30 15:36:03 +0800

    uni

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
-import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
+import 'v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import 'v3-core/contracts/libraries/TickMath.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
-import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
-import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
+import 'v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import 'v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
+import 'v3-periphery/contracts/libraries/TransferHelper.sol';
+import 'v3-periphery/contracts/base/LiquidityManagement.sol';
 
 contract LiquidityExamples is IERC721Receiver{
 
